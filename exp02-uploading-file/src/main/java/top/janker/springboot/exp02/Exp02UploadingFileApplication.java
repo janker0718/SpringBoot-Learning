636f7b7b55e8be50a76ad6ef7923e564c1fab295
janker <690864766@qq.com>
2022-02-17 09:27:58 +0800

    exp02

---
+++ exp02-uploading-file/src/main/java/top/janker/springboot/exp02/Exp02UploadingFileApplication.java
@@ -0,0 +1,13 @@
+package top.janker.springboot.exp02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exp02UploadingFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exp02UploadingFileApplication.class, args);
+    }
+
+}
---
+++ exp02-uploading-file/src/test/java/top/janker/springboot/exp02/Exp02UploadingFileApplicationTests.java
@@ -0,0 +1,13 @@
+package top.janker.springboot.exp02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exp02UploadingFileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

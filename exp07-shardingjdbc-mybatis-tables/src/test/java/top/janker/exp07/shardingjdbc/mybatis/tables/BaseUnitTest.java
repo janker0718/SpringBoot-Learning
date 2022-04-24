54cad6a01f81a4b56ddf85e809d34dd44e2065e1
janker <690864766@qq.com>
2022-04-24 10:23:25 +0800

    sharding-jdbc体验

---
+++ exp07-shardingjdbc-mybatis-tables/src/test/java/top/janker/exp07/shardingjdbc/mybatis/tables/BaseUnitTest.java
@@ -0,0 +1,10 @@
+package top.janker.exp07.shardingjdbc.mybatis.tables;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @date: 2022/4/22 10:48 下午
+ */
+@SpringBootTest
+public class BaseUnitTest {
+}

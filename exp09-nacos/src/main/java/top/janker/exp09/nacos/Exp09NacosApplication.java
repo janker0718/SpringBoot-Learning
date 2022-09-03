e5d8a29139792660559673a13423704a25109f33
janker0718 <690864766@qq.com@qq.com>
2022-09-04 00:16:11 +0800

    nacos springboot

---
+++ exp09-nacos/src/main/java/top/janker/exp09/nacos/Exp09NacosApplication.java
@@ -0,0 +1,13 @@
+package top.janker.exp09.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exp09NacosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exp09NacosApplication.class, args);
+    }
+
+}
---
+++ exp09-nacos/src/test/java/top/janker/exp09/nacos/Exp09NacosApplicationTests.java
@@ -0,0 +1,13 @@
+package top.janker.exp09.nacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exp09NacosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

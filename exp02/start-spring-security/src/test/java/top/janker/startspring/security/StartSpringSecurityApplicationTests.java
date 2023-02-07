5ea559a7fe7571169abb7030a6657acb40af5aae
janker0718 <690864766@qq.com@qq.com>
2023-02-07 09:06:34 +0800

    Spring Security 示例

---
+++ exp02/start-spring-security/src/test/java/top/janker/startspring/security/StartSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package top.janker.startspring.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
